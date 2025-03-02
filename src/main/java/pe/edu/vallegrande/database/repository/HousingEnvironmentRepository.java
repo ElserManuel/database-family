ade8f82b8e19cbc3c38db30150c69f7d4bc5b24b
ManuelLazaro <gorselazarorosas@gmail.com>
2025-03-02 16:49:16 -0500

    delet

--- src/main/java/pe/edu/vallegrande/database/repository/HousingEnvironmentRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.vallegrande.database.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import pe.edu.vallegrande.database.model.HousingEnvironment;
-
-public interface HousingEnvironmentRepository extends ReactiveCrudRepository<HousingEnvironment, Integer> {
-    
-}
--- src/main/java/pe/edu/vallegrande/database/repository/SocialLifeRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.vallegrande.database.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import pe.edu.vallegrande.database.model.SocialLife;
-
-public interface SocialLifeRepository extends ReactiveCrudRepository<SocialLife, Integer> {
-    
-}
